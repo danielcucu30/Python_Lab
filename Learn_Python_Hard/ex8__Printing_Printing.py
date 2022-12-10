971b84048ef3c8a519f14f55f34a906cf66cab94
Daniel Cucu <danielcucu34@gmail.com>
2022-12-10 05:27:46 -0500

    10_12_2022

--- Learn_Python_Hard/ex8__Printing_Printing.py
+++ Learn_Python_Hard/ex8__Printing_Printing.py
@@ -1,5 +1,17 @@
+ 
+from __future__ import print_function, unicode_literals
+
 formatter = "%r %r %r %r"
 
-print formatter % (1, 2, 3, 4)
+print (formatter % (1, 2, 3, 4))
 
-print formatter % ("one", "two", " tree", "four")
+print (formatter % ("one", "two", " tree", "four"))
+
+print (formatter % (True,False,True,False))
+
+print ((formatter,formatter,formatter,formatter))
+print (formatter % (" This is a paragah first line",
+"This is a paragah second line",
+"This is a paragah third line",
+"This is a paragah fourth line"
+))
